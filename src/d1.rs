69df2ad7625ff25cd4bd998b6cf93eb90fc18402
Vyacheslav Tyrtov <v.tyrtov@gmail.com>
2023-12-02 16:45:46 +0000

    Day 1 part 1
    Signed-off-by: Vyacheslav Tyrtov <v.tyrtov@gmail.com>

--- src/d1.rs
+++ src/d1.rs
@@ -1,61 +1,36 @@
-//pub trait Solve {
-//    fn new() -> Self;
-//    fn process(&mut self, inp: &String);
-//    fn result(&mut self) -> String;
-//}
-//
-
 #[path = "solve.rs"] pub mod solve;
 
 pub struct D1 {
-    curr: u64,
-    max1: u64,
-    max2: u64,
-    max3: u64,
-}
-
-impl D1 {
-    fn update_max(&mut self)
-    {
-        if self.max1 < self.curr {
-            self.max3 = self.max2;
-            self.max2 = self.max1;
-            self.max1 = self.curr;
-            self.curr = 0;
-        }
-        if self.max2 < self.curr {
-            self.max3 = self.max2;
-            self.max2 = self.curr;
-            self.curr = 0;
-        }
-        if self.max3 < self.curr {
-            self.max3 = self.curr;
-        }
-
-        self.curr = 0;
-    }
+    sum: u64,
 }
 
 impl solve::Solve for D1 {
     fn new() -> D1 
     {
-        D1 { curr: 0, max1: 0, max2: 0, max3: 0 }
+        D1 { sum: 0 }
     }
 
     fn process(&mut self, inp: &String)
     {
-        if inp.is_empty() {
-            self.update_max();
-        } else {
-            let cal: u64 = inp.parse().unwrap();
-            self.curr += cal;
+        let mut left: u64 = 0;
+        let mut right: u64 = 0;
+
+        for c in inp.chars() {
+            if c.is_digit(10) {
+                if left == 0{
+                    left = c.to_digit(10).unwrap().into();
+                }
+                right = c.to_digit(10).unwrap().into();
+            }
         }
+        let res = left * 10 + right;
+        self.sum += res;
+
+        println!("Res = {}", res);
     }
 
     fn result(&mut self) -> String
     {
-       self.update_max(); 
-       let max = self.max1 + self.max2 + self.max3;
-       return max.to_string();
+       return self.sum.to_string();
     }
 }
