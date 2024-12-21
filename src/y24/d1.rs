ad364a6d90f6e07791c954fd803e253bd897a8a5
Vyacheslav Tyrtov <v.tyrtov@gmail.com>
2024-12-21 19:23:40 +0000

    2024 Day 1 part 2
    Signed-off-by: Vyacheslav Tyrtov <v.tyrtov@gmail.com>

--- src/y24/d1.rs
+++ src/y24/d1.rs
@@ -2,12 +2,13 @@ use nom::character::complete::{digit1, space1};
 use nom::combinator::map_res;
 use nom::sequence::separated_pair;
 use nom::IResult;
+use std::collections::HashMap;
 use std::vec::Vec;
 #[path = "../solve.rs"] pub mod solve;
 
 pub struct Dx {
     l1: Vec<u64>,
-    l2: Vec<u64>,
+    l2: HashMap<u64, u64>,
 }
 
 fn to_u64<'a>(inp: &'a str) -> IResult<&'a str, u64> {
@@ -17,7 +18,7 @@ fn to_u64<'a>(inp: &'a str) -> IResult<&'a str, u64> {
 impl solve::Solve for Dx {
     fn new() -> Dx 
     {
-        Dx { l1: Vec::new(), l2: Vec::new() }
+        Dx { l1: Vec::new(), l2: HashMap::new() }
     }
 
     fn process(&mut self, inp: &String)
@@ -25,7 +26,10 @@ impl solve::Solve for Dx {
         match separated_pair(to_u64, space1, to_u64)(inp.as_str()) {
             Ok((_, (n1, n2))) => {
                 self.l1.push(n1);
-                self.l2.push(n2);
+                match self.l2.get_mut(&n2) {
+                    Some(val) => *val += 1,
+                    None => { self.l2.insert(n2, 1);},
+                }
             },
             Err(_) => panic!("Parsing error")
         }
@@ -36,9 +40,9 @@ impl solve::Solve for Dx {
        
         let mut sum = 0;
         self.l1.sort();
-        self.l2.sort();
         for idx in 0..self.l1.len() {
-            let v = (self.l1[idx] as i64 - self.l2[idx] as i64).abs();
+            let num = self.l1[idx];
+            let v = num * self.l2.get(&(num as u64)).unwrap_or(&0);
             sum += v;
         }
         return sum.to_string();
